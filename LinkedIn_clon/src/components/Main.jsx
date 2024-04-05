3dda031e9e66608ec094f3d651f7b398eedca708
Manoranjan1k <manasj790@gmail.com>
2024-04-05 22:25:23 +0530

    Main Updated

--- LinkedIn_clon/src/components/Main.jsx
+++ LinkedIn_clon/src/components/Main.jsx
@@ -33,7 +33,34 @@ const Main = (props) => {
       </button>
      </div>
      </ShareBox>
-     <Article>Article</Article>
+     <Article>
+       <SharedActor>
+        <a>
+          <img src="/images/user.svg" alt="" />
+          <div>
+            <span>Title</span>
+            <span>Info</span>
+            <span>Date</span>
+          </div>
+        </a>
+        <button>
+          <img src="/images/ellipsis.png" alt="" />
+        </button>
+       </SharedActor>
+       <Description>Description</Description>
+       <SharedImg>
+         <a>
+         <img src="/images/nav-network.svg" alt="" />
+         </a>
+       </SharedImg>
+       <SociolCounts>
+        <li>
+          <button>
+            <img src="" alt="" />
+          </button>
+        </li>
+       </SociolCounts>
+     </Article>
    </Container>
   );
   
@@ -111,8 +138,76 @@ const ShareBox = styled(CommonCard)`
 const Article = styled(CommonCard)`
      padding: 0;
      margin: 0 0 8px;
-     overflow: hidden;
+     overflow: visible;
 
 `;
+const SharedActor = styled.div`
+    padding-right: 40px;
+    flex-wrap: nowrap;
+    padding: 12px 16px 0;
+    margin-bottom: 8px;
+    align-items: center;
+    display: flex;
+    a{
+      margin-right:12px;
+      flex-grow: 1;
+      overflow: hidden;
+      display: flex;
+      text-decoration: none;
+
+      img{
+        width: 48px;
+        height: 48px;
+      }
+      & > div{
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        flex-bassis: 0;
+        margin-left: 8px;
+        overflow: hidden;
+        span{
+          text-align: left;
+          &:first-child{
+            font-size: 14px;
+            font-weight: 700;
+            color: rgba(0, 0, 0, 1);
+          }
+          &:nth-child(n + 1){
+            font-size: 12px;
+            color: rgba(0, 0, 0, 0.6);
+          }
+        }
+      }
+    }
+
+    button{
+      position: absolute; 
+      right: 12px;
+      top: 0;
+      background:transparent;
+      border:none;
+      outline: none;
+    }
+`;
+  const Description = styled.div`
+    padding: 0 16px;
+    overflow: hidden;
+    color: rgba(0, 0, 0, 0.9);
+    font-size: 14px;
+    text-align: left;
+  `
+  const SharedImg = styled.div`
+    margin-top: 8px;
+    width:100%;
+    display: block;
+    position: relative;
+    background-color: #f9fafb;
+    img{
+       object-fit: contain;
+       width: 100%;
+       height: 100%;
+    }
+  `
 
 export default Main;
