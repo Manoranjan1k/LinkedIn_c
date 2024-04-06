8af23f3527861486d1c829b2bcc2f19eaca45521
Manoranjan1k <manasj790@gmail.com>
2024-04-06 17:41:51 +0530

    Main updated

--- LinkedIn_clon/src/components/Main.jsx
+++ LinkedIn_clon/src/components/Main.jsx
@@ -56,10 +56,38 @@ const Main = (props) => {
        <SociolCounts>
         <li>
           <button>
-            <img src="" alt="" />
+            <img src="https://static.licdn.com/aero-v1/sc/h/2uxqgankkcxm505qn812vqyss" alt="" />
+
+            <img src="https://static.licdn.com/aero-v1/sc/h/cm8d2ytayynyhw5ieaare0tl3" alt="" />
+            <span>75</span>
           </button>
         </li>
+        <li>
+          <a>2 comment</a>
+        </li>
        </SociolCounts>
+
+       <SocialAction>
+      <button>
+        <img src="/images/like-i.png" alt="" />
+        <span>Like</span>
+      </button>
+
+      <button>
+        <img src="/images/Comments-icon.png" alt="" />
+        <span>Comment</span>
+      </button>
+
+      <button>
+        <img src="/images/forward.png" alt="" />
+        <span>Share</span>
+      </button>
+
+      <button>
+        <img src="/images/send.png" alt="" />
+        <span>Send</span>
+      </button>
+      </SocialAction>
      </Article>
    </Container>
   );
@@ -209,5 +237,42 @@ const SharedActor = styled.div`
        height: 100%;
     }
   `
+  const SociolCounts = styled.ul`
+      line-height: 1.3;
+      display: flex;
+      align-items: flex-start;
+      overflow: auto;
+      margin: 0 16px;
+      padding: 8px 0;
+      border-bottom: 1px solid #e9e5df;
+      list-style: none;
+      li{
+        margin-right: 5px;
+        font-size: 12px;
+        button{
+          
+          display: flex;
+          align-items: center;
+        }
+      }
+  `;
+    const SocialAction = styled.div`
+        align-items: center;
+        display: flex;
+        justify-content: flex-start;
+        margin: 0 ;
+        min-height: 40px;
+        padding: 4px 8px;
+        button{
+          display: inline-flex;
+          align-items: center;
+          padding: 8px;
+          color: #0a66c2;
 
+          @media(min-width: 768px){
+            span{
+              margin-left: 8px;
+            }
+        }
+    `;
 export default Main;
